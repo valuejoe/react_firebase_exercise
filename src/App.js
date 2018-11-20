c30dfe75b8acbd139eea6bdbf2bf03ca382e35c0
valuejoe <chiaohung82@gmail.com>
2018-11-20 16:59:19 +0800

    setting up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import {BrowserRouter} from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
+        <h1>HaHaapp</h1>
       </div>
+      </BrowserRouter>
     );
   }
 }
