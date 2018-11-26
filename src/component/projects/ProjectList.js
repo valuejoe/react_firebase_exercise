c04d5a55b130e465e4b33629d338097ceb0c8998
valuejoe <chiaohung82@gmail.com>
2018-11-26 15:44:15 +0800

    project details data with firebase

--- src/component/projects/ProjectList.js
+++ src/component/projects/ProjectList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
+import { Link } from 'react-router-dom'
 
 const ProjectList = ({projects}) => {
   return (
@@ -7,7 +8,9 @@ const ProjectList = ({projects}) => {
       {/* 如果有projects則執行projects.map() */}
       {projects && projects.map(project => {
         return(
-          <ProjectSummary project={project} key={project.id} />
+          <Link to={'/project/' + project.id}  key={project.id}>
+            <ProjectSummary project={project} />
+          </Link>
         )
       })}
     </div>
